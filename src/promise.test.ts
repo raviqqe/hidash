fc6d29b44393788dff46b36fa1b8cd471c11bb32
Yota Toyama <raviqqe@gmail.com>
2023-03-25 01:00:41 +1100

    Fix `flatSlice` indexing (#16)
    - Fix bug
    - Fix
    - Fix bug

--- src/promise.test.ts
+++ src/promise.test.ts
@@ -186,6 +186,31 @@ describe(flatSlice.name, () => {
 
     expect(await toArray(flatSlice(createIterable(), 1, 2))).toEqual([[2]]);
   });
+
+  it("slices an iterable with four elements in two chunks", async () => {
+    const createIterable = async function* () {
+      yield [1, 2];
+      yield [3, 4];
+    };
+
+    expect(await toArray(flatSlice(createIterable(), 0, 0))).toEqual([]);
+    expect(await toArray(flatSlice(createIterable(), 0, 1))).toEqual([[1]]);
+    expect(await toArray(flatSlice(createIterable(), 1, 1))).toEqual([]);
+    expect(await toArray(flatSlice(createIterable(), 1, 2))).toEqual([[2]]);
+    expect(await toArray(flatSlice(createIterable(), 1, 3))).toEqual([
+      [2],
+      [3],
+    ]);
+    expect(await toArray(flatSlice(createIterable(), 1, 4))).toEqual([
+      [2],
+      [3, 4],
+    ]);
+    expect(await toArray(flatSlice(createIterable(), 2, 2))).toEqual([]);
+    expect(await toArray(flatSlice(createIterable(), 2, 3))).toEqual([[3]]);
+    expect(await toArray(flatSlice(createIterable(), 2, 4))).toEqual([[3, 4]]);
+    expect(await toArray(flatSlice(createIterable(), 3, 3))).toEqual([]);
+    expect(await toArray(flatSlice(createIterable(), 3, 4))).toEqual([[4]]);
+  });
 });
 
 describe(filter.name, () => {
