c9108f80f95895a75857353961bb1159b42a2d48
Yota Toyama <raviqqe@gmail.com>
2023-03-25 00:53:46 +1100

    Fix bug

--- src/promise.test.ts
+++ src/promise.test.ts
@@ -186,6 +186,22 @@ describe(flatSlice.name, () => {
 
     expect(await toArray(flatSlice(createIterable(), 1, 2))).toEqual([[2]]);
   });
+
+  it("slices an iterable with four elements in two chunks", async () => {
+    const createIterable = async function* () {
+      yield [1, 2];
+      yield [3, 4];
+    };
+
+    expect(await toArray(flatSlice(createIterable(), 0, 0))).toEqual([]);
+    expect(await toArray(flatSlice(createIterable(), 0, 1))).toEqual([[1]]);
+    expect(await toArray(flatSlice(createIterable(), 1, 1))).toEqual([]);
+    expect(await toArray(flatSlice(createIterable(), 1, 2))).toEqual([[2]]);
+    expect(await toArray(flatSlice(createIterable(), 1, 3))).toEqual([[2, 3]]);
+    expect(await toArray(flatSlice(createIterable(), 1, 4))).toEqual([
+      [2, 3, 4],
+    ]);
+  });
 });
 
 describe(filter.name, () => {
