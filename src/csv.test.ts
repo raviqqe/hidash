479895c5cc34d4d0b240335c59a7f26b9ec9e78e
Yota Toyama <raviqqe@gmail.com>
2023-05-19 12:03:53 +1000

    Fix

---
+++ src/csv.test.ts
@@ -0,0 +1,90 @@
+import { expect, it } from "vitest";
+import { parse } from "./csv.js";
+import { toArray } from "./promise.js";
+
+it("parses nothing", async () => {
+  expect(
+    await toArray(
+      parse(
+        (async function* () {
+          yield "";
+        })()
+      )
+    )
+  ).toEqual([]);
+});
+
+it("parses a JSON", async () => {
+  expect(
+    await toArray(
+      parse(
+        (async function* () {
+          yield "{}";
+        })()
+      )
+    )
+  ).toEqual([{}]);
+});
+
+it("parses a JSON with a trailing newline", async () => {
+  expect(
+    await toArray(
+      parse(
+        (async function* () {
+          yield "{}\n";
+        })()
+      )
+    )
+  ).toEqual([{}]);
+});
+
+it("parses two JSONs", async () => {
+  expect(
+    await toArray(
+      parse(
+        (async function* () {
+          yield "{}\n{}";
+        })()
+      )
+    )
+  ).toEqual([{}, {}]);
+});
+
+it("parses two JSONs in different yields", async () => {
+  expect(
+    await toArray(
+      parse(
+        (async function* () {
+          yield "{}\n{";
+          yield "}";
+        })()
+      )
+    )
+  ).toEqual([{}, {}]);
+});
+
+it("parses two JSONs with a newline delta", async () => {
+  expect(
+    await toArray(
+      parse(
+        (async function* () {
+          yield "{}";
+          yield "\n";
+          yield "{}";
+        })()
+      )
+    )
+  ).toEqual([{}, {}]);
+});
+
+it("parses three JSONs", async () => {
+  expect(
+    await toArray(
+      parse(
+        (async function* () {
+          yield "{}\n{}\n{}";
+        })()
+      )
+    )
+  ).toEqual([{}, {}, {}]);
+});
