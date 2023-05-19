13d705e69f155aeb5e2e4b68f834ee21127cbf12
Yota Toyama <raviqqe@gmail.com>
2023-05-19 11:33:55 +1000

    Add line parser

--- src/string.ts
+++ src/string.ts
@@ -1,3 +1,5 @@
+import { last } from "lodash-unified";
+
 export const isAlphabetic = (text: string): boolean =>
   [...text].every(
     (character) => character.toLowerCase() !== character.toUpperCase()
@@ -5,3 +7,29 @@ export const isAlphabetic = (text: string): boolean =>
 
 export const capitalizeText = (text: string): string =>
   (text[0]?.toUpperCase() ?? "") + text.slice(1);
+
+export const parseLines = async function* (
+  iterable: AsyncIterable<string>
+): AsyncIterable<string> {
+  let line = "";
+
+  for await (const delta of iterable) {
+    const [former = "", ...rest] = delta.split("\n");
+
+    if (typeof last(rest) === "string") {
+      yield line + former;
+
+      for (const line of rest.slice(0, -1)) {
+        yield line;
+      }
+
+      line = last(rest) ?? "";
+    } else {
+      line += former;
+    }
+  }
+
+  if (line) {
+    yield line;
+  }
+};
