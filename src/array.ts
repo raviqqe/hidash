ed7ef4ee0390aae4f3e16f0459f4c65ce3ca24cc
Yota Toyama <raviqqe@gmail.com>
2023-06-03 15:44:20 +1000

    Extend interleave function (#97)

--- src/array.ts
+++ src/array.ts
@@ -1,11 +1,14 @@
-export const interleave = <T, S>(xs: T[], separator: S): (T | S)[] => {
+export const interleave = <T, S>(
+  xs: T[],
+  separator: S | ((index: number) => S)
+): (T | S)[] => {
   const ys = [];
 
   for (const [index, value] of xs.entries()) {
     ys.push(value);
 
     if (index !== xs.length - 1) {
-      ys.push(separator);
+      ys.push(separator instanceof Function ? separator(index) : separator);
     }
   }
 
