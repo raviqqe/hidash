d79714c55c846c3b39c26a1cf3031cdfec75ce9a
Yota Toyama <raviqqe@gmail.com>
2023-06-03 15:43:37 +1000

    Extend interleave function

--- src/array.test.ts
+++ src/array.test.ts
@@ -8,4 +8,10 @@ describe(interleave.name, () => {
     expect(interleave([1, 2], null)).toEqual([1, null, 2]);
     expect(interleave([1, 2, 3], null)).toEqual([1, null, 2, null, 3]);
   });
+
+  it("interleaves a separator with a function", async () => {
+    expect(interleave([1, 2, 3], (index) => index + 42)).toEqual([
+      1, 42, 2, 43, 3,
+    ]);
+  });
 });
