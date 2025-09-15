3f3c1b498e1c935cb94f768590f572f85e018a24
Yota Toyama <raviqqe@gmail.com>
2025-09-14 19:33:09 -0700

    Fix

--- src/string.ts
+++ src/string.ts
@@ -1,6 +1,4 @@
-import { last } from "@raviqqe/loscore";
-
-export const parseLines = async function* (
+export const parseLines = async function*(
   iterable: AsyncIterable<string>,
 ): AsyncIterable<string> {
   let line = "";
@@ -8,20 +6,20 @@ export const parseLines = async function* (
   for await (const delta of iterable) {
     const [former = "", ...rest] = delta.split("\n");
 
-    if (typeof last(rest) === "string") {
+    if (typeof rest.at(-1) === "string") {
       yield line + former;
 
       for (const line of rest.slice(0, -1)) {
         yield line;
       }
 
-      line = last(rest) ?? "";
+      line = rest.at(-1) ?? "";
     } else {
       line += former;
     }
   }
 
-  if (line) {
-    yield line;
-  }
+if (line) {
+  yield line;
+}
 };
